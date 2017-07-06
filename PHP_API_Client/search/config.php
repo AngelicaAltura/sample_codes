b4a110d8af1829d5831af8645baa205a8d46a354
AngelicaAltura <aaltura@zendesk.com>
2017-07-07 01:08:11 +0800

    Update config.php

--- PHP_API_Client/search/config.php
+++ PHP_API_Client/search/config.php
@@ -4,4 +4,6 @@
 define("ZDAPIKEY", "apbhrXx8UirqnqNMlfseS4N84NBw9wXjuYcbpXCM");
 define("ZDUSER", "anhelika@mymelody.com");
 define("ZSUB", "z3nmanilaaltura");
+define("EXTERNALID", "012345");
+define("TICKETSTATUS", "open");
  ?>
