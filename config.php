a3312aded6be8a11bff3c313acd89595e5934818
AngelicaAltura <aaltura@zendesk.com>
2017-07-07 00:52:14 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+//General configuration for PhP API Client
+
+<?php
+define("ZDAPIKEY", "apbhrXx8UirqnqNMlfseS4N84NBw9wXjuYcbpXCM");
+define("ZDUSER", "anhelika@mymelody.com");
+define("ZSUB", "z3nmanilaaltura");
+ ?>
