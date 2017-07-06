c1ff0560916628bd1add0d03c5043b2026f05649
AngelicaAltura <aaltura@zendesk.com>
2017-07-07 01:07:42 +0800

    Update search.php

--- PHP_API_Client/search/search.php
+++ PHP_API_Client/search/search.php
@@ -17,26 +17,20 @@ $client->setAuth('basic', ['username' => $username, 'token' => $token]);
 
 try {
     // Search the current customer
-    $params = ['query' =>'type:ticket status:open'];
-    $search = $client->search()->find('type:ticket external_id:012345');
+    $params = ['query' =>'type:ticket status:' . TICKETSTATUS];
+    $search = $client->search()->find('type:ticket external_id:' . EXTERNALID);
     $to_dump ="";
 	
-	var_dump($search);
+	//var_dump($search);
 	
     if (empty($search->results)) {
         var_dump('0 results');
 	} else {
+           var_dump(count($search) . ' results');
 	       foreach ($search->results as $ticketData) {
-                 $to_dump = $to_dump . $ticketData->id . " results" . " <br>";
-				/**echo "success";
-	            $userId = $ticketData->id;	
-
-	            // Show the results
-	            echo "<pre>";
-	            print_r($userId);
-	            echo "</pre>";**/
+                 var_dump($ticketData->id);
            }
-        var_dump($to_dump);
+       
 	}
     
 } catch (\Zendesk\API\Exceptions\ApiResponseException $e) {
